0c36bae3576956835b23b26712e718f29fea85ec
darento <dsartiom@gmail.com>
2023-12-29 12:43:28 +0100

    Added tqdm in case we want progress bar

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 from typing import List
+from tqdm   import tqdm
+import time
 
 def estimate_remaining_time(iteration_times: List[float], 
                             total_iterations: int, 
@@ -12,3 +14,10 @@ def estimate_remaining_time(iteration_times: List[float],
     estimated_remaining_time = avg_time_per_iteration * remaining_iterations
 
     print(f"Estimated remaining time for {string_process}: {round(estimated_remaining_time, 1)} seconds")
+    
+def progress_bar(total_time: float) -> tqdm:
+    # Create a progress bar
+    with tqdm(total=total_time, desc="Progress", bar_format="{l_bar}{bar}") as pbar:
+        for i in range(int(total_time)):
+            time.sleep(1)  # Sleep for 1 second
+            pbar.update(1)  # Update the progress bar
