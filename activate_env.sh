2cee92cfa4cb0e9ab7ea7050297349f9e40194f7
darento <dsartiom@gmail.com>
2024-01-09 23:05:44 +0100

    .sh files reorganized, only need to source make_condaEnv.sh now

--- activate_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CONDA_ENV_NAME=env_PETsys
-YML_FILENAME=${CONDA_ENV_NAME}.yml
-source /home/metamaterials/miniconda/bin/activate ${CONDA_ENV_NAME}
-python setup.py develop
-echo "Environment activated."
-echo "To deactivate type: conda deactivate"
